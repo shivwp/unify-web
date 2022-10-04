99156728e4d83560a261c69f4db09419bb60c03d
AjayAtEoxysIT <ajay@eoxysit.com>
2022-10-04 17:14:13 +0530

    my_report page added

---
+++ src/freelancer/UserReports.js
@@ -0,0 +1,15 @@
+import NavbarLogin from "./components/header";
+import Footer from "../components/footer";
+import Reports from "./Reports";
+
+const MyReport = () => {
+  return (
+    <>
+      <NavbarLogin />
+      <Reports />
+      <Footer />
+    </>
+  );
+};
+
+export default MyReport;
