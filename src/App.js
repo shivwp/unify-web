b79c3ef2fea5ff37b6995bcbfaabad7cf4ece51b
AjayAtEoxysIT <ajay@eoxysit.com>
2022-12-10 11:50:00 +0530

    session storage set when image change

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import ClientRoutes from "./routing/ClientRoutes";
 import CommanRoutes from "./routing/CommanRoutes";
 import FreelancerRoutes from "./routing/FreelancerRoutes";
 const FooterPages = lazy(() => import("./FooterPages"));
+const Subscription = lazy(() => import("./client/Pages/Subscription"));
 
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <CommanRoutes />
       <Routes>
+        <Route path="/subscription" element={<Subscription />} />
+
         <Route path="/pages/:pagename" element={<FooterPages />} />
       </Routes>
       <ClientRoutes />
